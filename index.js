e602a6f47798905e249b13f5a8427ddeb84bff06
Ksenia Lipkina <lipkina666@gmail.com>
2025-05-22 10:37:12 +0300

    Check

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+import './index.css'
+import enduroLogo from './assets/enduro-logo.svg';
+
+const title = document.createElement("h1");
+title.textContent = 'I love Enduro';
+
+const img = document.createElement("img");
+img.src = enduroLogo;
+
+document.body.append(title)
+document.body.append(img)
+
 console.log('Hello World!')
--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,7 +1,20 @@
+import babel from "@rollup/plugin-babel"
+import styles from "rollup-plugin-styles"
+import image from "@rollup/plugin-image"
+import serve from 'rollup-plugin-serve'
+import livereload from 'rollup-plugin-livereload'
+
 export default {
     input: './index.js',
     output: {
         file: './dist/bundle.js',
         format: 'cjs'
-    }
+    },
+    plugins: [
+        babel({ babelHelpers: 'bundled' }),
+        styles(),
+        image(),
+        serve(),
+        livereload()
+    ]
 };
